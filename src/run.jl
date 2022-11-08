c9bda5ca8e4a5845a1a3ed122e4e88ccfec03a0f
Junyaokk <junyaoyu@163.sufe.edu.cn>
2022-11-08 13:35:06 +0800

    run

--- src/run.jl
+++ src/run.jl
@@ -1,6 +1,6 @@
 path = joinpath(dirname(@__FILE__), "", "")
 push!(Base.LOAD_PATH, path)
-print(path)
+
 using Benders
 Bender.milp(n_x, n_y, vec_min_y, vec_max_y, vec_c, vec_f,
   vec_b, mat_a, mat_b, epsilon, timesIterationMax)
